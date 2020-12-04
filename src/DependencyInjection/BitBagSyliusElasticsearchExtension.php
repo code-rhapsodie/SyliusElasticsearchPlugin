b20880010275674f46bc513de49a31fe6ea8fc21
Marc DAVID <marc@Mac.local>
2020-12-04 15:21:53 +0100

    Remove Lines with dumps

--- src/DependencyInjection/BitBagSyliusElasticsearchExtension.php
+++ src/DependencyInjection/BitBagSyliusElasticsearchExtension.php
@@ -27,7 +27,6 @@ final class BitBagSyliusElasticsearchExtension extends Extension implements Prep
 
     public function prepend(ContainerBuilder $container)
     {
-       // dump("je passe dans preprend");
         $configs = $container->getExtensionConfig($this->getAlias());
         $config = $this->processConfiguration(new Configuration(), $configs);
         $container->setParameter('bitbag_es_filter_attributes_max', $config['filter_attributes_max']);
@@ -35,8 +34,5 @@ final class BitBagSyliusElasticsearchExtension extends Extension implements Prep
         $container->setParameter('bitbag_es_shop_name_property_prefix', $config['shop_name_property_prefix']);
         $container->setParameter('bitbag_es_excluded_filter_options', $config['excluded_filter']['options']);
         $container->setParameter('bitbag_es_excluded_filter_attributes', $config['excluded_filter']['attributes']);
-        dump($configs);
-        dump($config);
-        //$container = prependExtensionConfig($name, $config);
     }
 }
